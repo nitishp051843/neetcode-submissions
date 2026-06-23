3b8254572b73982097cf266b777a5e8f270c0032
Nitish Kumar P <nitishp051843@gmail.com>
2026-06-24 00:07:12 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mapping = {}
+        for val in strs:
+            key = tuple(sorted(val))
+            if key in mapping:
+                mapping[key].append(val)
+            else:
+                mapping[key] = [val]
+        
+        return [i for i in mapping.values()]
